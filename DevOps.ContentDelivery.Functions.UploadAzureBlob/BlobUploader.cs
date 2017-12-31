c020b95e8e95892f55f1504ec4a74a9001fdf4d8
Christopher Dorst <chris@dorst.email>
2017-12-31 01:55:05 -0600

    Add BlobUploader

---
+++ DevOps.ContentDelivery.Functions.UploadAzureBlob/BlobUploader.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+using static System.IO.File;
+
+namespace DevOps.ContentDelivery.Functions.UploadAzureBlob
+{
+    public static class BlobUploader
+    {
+        public static async Task Upload(CloudBlobContainer container, string name, string path)
+        {
+            var blob = container.GetBlockBlobReference(name);
+            using (var stream = OpenRead(path))
+                await blob.UploadFromStreamAsync(stream);
+        }
+    }
+}
